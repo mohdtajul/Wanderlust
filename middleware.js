bc895ef10ef7c9b37bce8b50bb6cd579e7b6ed27
mohdtajul <mohdtajul473@gmail.com>
2026-01-19 18:17:12 +0530

    added some middlewares

--- middleware.js
+++ middleware.js
@@ -1,3 +1,6 @@
+const listing = require('./models/listing')
+const ExpressError = require('./utils/ExpressError.js')
+const {listingSchema,reviewSchema} = require('./schema.js')
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.redirectUrl = req.originalUrl;
@@ -12,4 +15,36 @@ module.exports.saveRedirectUrl=(req,res,next)=>{
     res.locals.redirectUrl= req.session.redirectUrl;
   }
   next();
+}
+
+module.exports.isOwner = async (req,res,next)=>{
+    let {id}= req.params
+    // let {listing} = req.body
+    let listing = await Listing.findById(id);
+    if(!listing.owner.equals(currUser._id)){
+         req.flash("error", "You have no permission to edit this one")
+        return res.redirect(`/listing/${id}`)
+    }
+    next()
+}
+
+module.exports.validateListing = (req,res,next) =>{
+ 
+    const { error } = listingSchema.validate(req.body);
+
+    if(error){
+        const errMsg = error.details.map(el => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    next();
+}
+
+module.exports.validateReview = (req,res,next)=>{
+    const { error } = reviewSchema.validate(req.body);
+
+    if(error){
+        const errMsg = error.details.map(el => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    next();
 }
