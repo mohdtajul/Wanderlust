ccc35ad2888efe521b12a6cbe8507912831cfe3a
mohdtajul <mohdtajul473@gmail.com>
2026-01-19 18:18:35 +0530

    made some basic changes

--- routes/review.js
+++ routes/review.js
@@ -2,20 +2,9 @@ const express = require("express")
 const router = express.Router({mergeParams: true})
 const wrapAsync = require("../utils/wrapAsync.js")
 const ExpressError = require("../utils/ExpressError.js")
-const {reviewSchema} = require("../schema.js")
 const Review = require("../models/reviews.js")
 const Listing = require("../models/listing.js") // model imported
-
-
-const validateReview = (req,res,next)=>{
-    const { error } = reviewSchema.validate(req.body);
-
-    if(error){
-        const errMsg = error.details.map(el => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }
-    next();
-}
+const {validateReview} = require('../middleware.js')
 
 //  Post reviews route
 router.post("/",validateReview,wrapAsync(async(req,res)=>{
