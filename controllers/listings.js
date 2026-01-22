8a4fa51797a1a0090113559d373d5abf5be081e1
mohdtajul <mohdtajul473@gmail.com>
2026-01-22 20:47:15 +0530

    added controllers for each routes

---
+++ controllers/listings.js
@@ -0,0 +1,87 @@
+const Listing = require('../models/listing.js')
+
+module.exports.index = async (req,res)=>{
+    const allListings = await Listing.find({}) // mongo se pura listings collection ka data liya h
+    res.render("listings/index.ejs",{allListings})
+
+    }
+
+module.exports.renderNewForm = (req,res)=>{
+    res.render("listings/newplace.ejs") // rendering form to get data for listing 
+}
+
+module.exports.showListing = async (req, res) => {
+  const listing = await Listing
+    .findById(req.params.id)
+    .populate({
+        path:"reviews",
+        populate:{
+            path: "author",
+        },
+    })
+    .populate("owner");
+
+  if (!listing) {
+    req.flash("error", "Listing you requested for doesn't exist!");
+    return res.redirect("/listings");
+  }
+
+  res.render("listings/show.ejs", { listing });
+}
+
+module.exports.createListing = async (req,res,next)=>{
+        const newListing = new Listing(req.body.listing);
+        newListing.owner=req.user._id;
+        await newListing.save();
+        req.flash("success", "New Listing Created")
+        res.redirect("/listings")
+}
+
+module.exports.renderEditForm = async (req,res)=>{
+    let {id} = req.params
+    const listing = await Listing.findById(id)
+    console.log(listing);
+    if(!listing){
+        req.flash("error", "Listing you requested for doesn't exist !")
+        res.redirect("/listings")
+    }else{
+        res.render("listings/edit.ejs",{listing})
+
+    }
+    
+}
+
+module.exports.updateListing = async (req, res) => {
+
+    const { id } = req.params;
+    const { listing } = req.body;
+
+    const updatedListing = await Listing.findByIdAndUpdate(
+      id,
+      {
+        title: listing.title,
+        description: listing.description,
+        image: { url: listing.image.url },
+        price: listing.price,
+        location: listing.location,
+        country: listing.country
+      },
+      { new: true }   // ⭐ IMPORTANT
+    );
+
+    if (!updatedListing) {
+      req.flash("error", "Listing not found");
+      return res.redirect("/listings");
+    }
+
+    req.flash("success", "Listing Updated Successfully");
+    res.redirect(`/listings/${updatedListing._id}`);
+  }
+
+  module.exports.destroyListing = async(req,res)=>{
+      let {id} = req.params
+      await Listing.findByIdAndDelete(id)
+      req.flash("success", "Listing Deleted")
+      res.redirect("/listings")
+  
+}
