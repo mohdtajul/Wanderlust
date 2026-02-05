72487adbc06dd68db1322d1fea8c797484ed8554
mohdtajul <mohdtajul473@gmail.com>
2026-02-05 11:31:36 +0530

    updated image in schema

--- models/listing.js
+++ models/listing.js
@@ -4,23 +4,13 @@ const Review = require('./reviews.js');
 // created schema for collection
 const listingSchema = new mongoose.Schema({
     title:{
-        type:String,
-        required:true,
+        type: String,
+        required: true,
     },
-    description:{
-        type:String,
-    },
+    description: String,
     image:{
-        filename:String,
-        url:{
-            type:String,
-            default:"https://cdn.pixabay.com/photo/2016/11/06/22/28/sundown-1807524_1280.jpg",
-            set: (v)=>
-                v===""
-                ?"https://cdn.pixabay.com/photo/2016/11/06/22/28/sundown-1807524_1280.jpg"
-
-                :v,
-        }
+        url: String,
+        filename: String,
     },
     price: Number,
     location: String,
