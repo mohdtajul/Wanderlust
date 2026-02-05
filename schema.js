d9d25fd47f6c2681ff94e29ca602b104502b7230
mohdtajul <mohdtajul473@gmail.com>
2026-02-05 11:33:59 +0530

    removed image

--- schema.js
+++ schema.js
@@ -6,9 +6,9 @@ module.exports.listingSchema = Joi.object({
         location: Joi.string().required(),
         country: Joi.string().required(),
         price: Joi.number().required().min(0),
-        image: Joi.object({
-            url: Joi.string().allow("", null)
-        }).required()
+        // image: Joi.object({
+        //     url: Joi.string().allow("", null)
+        // }).required()
 
     }).required()
 })
