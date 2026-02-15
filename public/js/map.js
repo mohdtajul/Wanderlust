364a5c4b52f5dacb895e957f6d78cbd5cd81df10
mohdtajul <mohdtajul473@gmail.com>
2026-02-15 15:34:05 +0530

    made changes

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,12 @@
-    mapboxgl.accessToken = mapToken;
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        center: [77.209, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9 // starting zoom
-    });
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: coordinates,   // 🔥 dynamic from EJS
+  zoom: 9
+});
+
+new mapboxgl.Marker({ color: "black" })
+  .setLngLat(coordinates)
+  .addTo(map);
